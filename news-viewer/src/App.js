9a9163b18d0cc7613c576f777bf6556938c2bdef
hamin <hm5172@naver.com>
2023-05-17 17:07:02 +0900

    뉴스 API 호출

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function App () {
+  // API를 넘겨받을 state 선언
+  const [data, setData ] = useState(null);
+
+  const onClick = async () => {
+    // axios 라이브러리로 apic call 
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=afda249a1ccb482fa0944d12a295021b',
+      );
+      // 응답 data state 저장
+      setData(response.data);
+    } catch (e) {
+      console.log(e)
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => onClick()}>불러오기</button>
+      </div>
+      {/* JSON 문자열 뿌릴 영역 */}
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
     </div>
   );
-}
+};
 
-export default App;
+export default App
