6fe9b14bd17347d1b45626cd853b235bd719b295
hamin <hm5172@naver.com>
2023-05-17 17:55:09 +0900

    feat: 뉴스 뷰어 만들기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import NewsItem  from "./NewsItem";
+import axios from "axios";
 
 const NewsItemBlock = styled.div`
     box-sizing: border-box;
@@ -15,24 +16,47 @@ const NewsItemBlock = styled.div`
     }
 `;
 
-const sampleAricle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoding] = useState(null);
 
-const newsList = () => {
+    useEffect(()=>{
+        //async 비동기 함수 호출
+        const fetchData = async ()=>{
+            //api 호출 시간동안 보여줄 로딩바
+            setLoding(true)
+            //try catch문 에러 처리
+            try{
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?country=kr&apiKey=afda249a1ccb482fa0944d12a295021b',
+                );
+                //api 데이터 state 저장
+                setArticles(response.data.articles)
+            }catch(e){
+                console.log(e)
+            }
+            setLoding(false)
+        };
+        fetchData();
+    },{});
+
+    //대기 중
+    if (loading){
+        return <NewsItemBlock>대기 중 입니다....</NewsItemBlock>
+    }
+    //articles 값이 설정 안될 경우
+    if(!articles){
+        return null;
+    }
+
+    //articles 같이 유효할 때
     return(
         <NewsItemBlock>
-            <NewsItem article={sampleAricle}/>
-            <NewsItem article={sampleAricle}/>
-            <NewsItem article={sampleAricle}/>
-            <NewsItem article={sampleAricle}/>
-            <NewsItem article={sampleAricle}/>
-            <NewsItem article={sampleAricle}/>
+            {articles.map(v=>(
+                <NewsItem key={v.url} article={v}/>
+                ))}
         </NewsItemBlock>
     );
 };
 
-export default newsList;
+export default NewsList;
